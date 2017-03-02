07b14806ed046af39a2c93f7a4fbb0fec519ad7c
root <root@localhost.localdomain>
2017-03-02 13:09:43 -0500

    Fix sytax. Remove sudo.

--- manifests/rdt.pp
+++ manifests/rdt.pp
@@ -1,14 +1,15 @@
 # == Class: ::barometer::rdt
 # RDT preparation: ldconfig and load msr module
 
-class ::barometer::rdt {
+class barometer::rdt (
+) {
   exec { 'update linker cache':
     path => '/usr/bin',
-    command => 'sudo ldconfig',
+    command => 'ldconfig',
   }
   exec { 'load MSR module':
     path => '/usr/bin',
-    command => 'sudo modprobe msr',
+    command => 'modprobe msr',
   }
 }
 
