9cbbadd0f41f417075b3c804bd82d35a2c3beec2
root <root@localhost.localdomain>
2017-03-07 17:11:31 -0500

    Get ceilometer credentials and pass to configuration script.

--- manifests/collectd.pp
+++ manifests/collectd.pp
@@ -2,7 +2,17 @@
 # Collectd service start 
 
 class barometer::collectd (
+  $public_url = hiera(ceilometer::keystone::auth::public_url),
+  $username   = hiera(ceilometer::keystone::authtoken::username),
+  $password   = hiera(ceilometer::keystone::authtoken::password),
 ) {
+  exec { 'show ceilometer creds':
+    path => /usr/sbin',
+    command => 'echo "$public_url $username $password" > /home/collectd-creds',
+  exec { 'collectd-ceilometer-plugin conf':
+    path => /usr/sbin',
+    command => 'bash /opt/collectd/collectd-conf.sh public_url, username, password > /home/collectd-conf-status',
+  }
   service { 'collectd':
     ensure => 'running',
     enable => true,
