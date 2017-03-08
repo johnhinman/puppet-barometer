6fc64fd64d1e3838504fde0eac623a16859bbaf6
root <root@localhost.localdomain>
2017-03-07 21:38:15 -0500

    Add missing single quotes.

--- manifests/collectd.pp
+++ manifests/collectd.pp
@@ -7,10 +7,10 @@ class barometer::collectd (
   $password   = hiera(ceilometer::keystone::authtoken::password),
 ) {
   exec { 'show ceilometer creds':
-    path => /usr/bin',
+    path => '/usr/bin',
     command => 'echo "$public_url $username $password" > /home/collectd-creds',
   exec { 'collectd-ceilometer-plugin conf':
-    path => /usr/bin',
+    path => '/usr/bin',
     command => 'bash /opt/collectd-conf.sh public_url, username, password > /home/collectd-conf-status',
   }
   service { 'collectd':
