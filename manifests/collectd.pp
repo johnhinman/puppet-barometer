34c0ad1f63dfdb1eb335c72f850daa6d5f1ca34c
root <root@localhost.localdomain>
2017-03-13 18:19:03 -0400

    Change parameter names and default values.

--- manifests/collectd.pp
+++ manifests/collectd.pp
@@ -2,17 +2,17 @@
 # Collectd service start 
 
 class barometer::collectd (
-  $public_url = hiera(ceilometer::keystone::auth::public_url),
-  $username   = hiera(ceilometer::keystone::authtoken::username),
-  $password   = hiera(ceilometer::keystone::authtoken::password),
+  $public_url,
+  $ceilo_username   = 'ceilometer',
+  $ceilo_password   = 'ceilometer-password,
 ) {
   exec { 'show ceilometer creds':
     path => '/usr/bin',
-    command => 'echo "$public_url $username $password" > /home/collectd-creds',
+    command => 'echo $public_url $ceilo_username $ceilo_password > /home/collectd-creds',
   }
   exec { 'collectd-ceilometer-plugin conf':
     path => '/usr/bin',
-    command => 'bash /opt/collectd-conf.sh public_url, username, password > /home/collectd-conf-status',
+    command => 'bash /opt/collectd-conf.sh $public_url, $ceilo_username, $ceilo_password > /home/collectd-conf-status',
   }
   service { 'collectd':
     ensure => 'running',
