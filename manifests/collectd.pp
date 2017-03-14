bb585fd6c439a3c8d16ebc7c68c0f3c2baa7968d
root <root@localhost.localdomain>
2017-03-14 19:13:49 -0400

    Modify echo to log.

--- manifests/collectd.pp
+++ manifests/collectd.pp
@@ -8,11 +8,12 @@ class barometer::collectd (
 ) {
   exec { 'show ceilometer creds':
     path => '/usr/bin',
-    command => 'echo $public_url $ceilo_username $ceilo_password > /home/collectd-creds',
+    command => 'echo BAROMETER: $public_url',
+#    command => 'echo $public_url $ceilo_username $ceilo_password > /home/collectd-creds',
   }
   exec { 'collectd-ceilometer-plugin conf':
     path => '/usr/bin',
-    command => 'bash /opt/collectd-conf.sh $public_url, $ceilo_username, $ceilo_password > /home/collectd-conf-status',
+    command => 'bash /opt/collectd-conf.sh $public_url $ceilo_username $ceilo_password > /home/collectd-conf-status',
   }
   service { 'collectd':
     ensure => 'running',
