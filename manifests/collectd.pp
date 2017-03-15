df49cf97792614d0d0a5a8c9c03cd8a9f679e4bd
root <root@localhost.localdomain>
2017-03-14 20:50:11 -0400

    Try hiera('barometer::collectd::password').

--- manifests/collectd.pp
+++ manifests/collectd.pp
@@ -4,12 +4,11 @@
 class barometer::collectd (
   $public_url = barometer::collectd::public_url,
   $ceilo_username = barometer::collectd::ceilo_username,
-  $ceilo_password = barometer::collectd::ceilo_password,
+  $ceilo_password = hiera('barometer::collectd::ceilo_password'),
 ) {
   exec { 'show ceilometer creds':
     path => '/usr/bin',
-    command => 'echo BAROMETER: $public_url',
-#    command => 'echo $public_url $ceilo_username $ceilo_password > /home/collectd-creds',
+    command => 'echo barometer $public_url $ceilo_username $ceilo_password > /home/collectd-creds',
   }
   exec { 'collectd-ceilometer-plugin conf':
     path => '/usr/bin',
